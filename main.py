e3b23cd9763068efcbc1f0c82a2b3ae7da9d4fbc
Irving Rodríguez <irving.prog@gmail.com>
2014-07-02 22:39:26 -0500

    Changing screen size and code improvement

--- main.py
+++ main.py
@@ -47,9 +47,9 @@ class GameScene(object):
     def __init__(self):
         self.cells = []
 
-        self.witdth_tiled = 64
-        self.height_tiled = 48
-        size_tile = 640 / self.witdth_tiled
+        self.witdth_tiled = 80
+        self.height_tiled = 60
+        size_tile = 800 / self.witdth_tiled
 
         for f in range(self.height_tiled):
             for c in range(self.witdth_tiled):
@@ -57,7 +57,7 @@ class GameScene(object):
                                size_tile, size_tile)
                 self.cells.append(Cell(rect, (f, c)))
 
-        self.status = False
+        self.auto_generation = False
 
         gosper_glider = [(6, 31), (7, 29), (7, 31), (8, 19), (8, 20),
                          (8, 27), (8, 28), (8, 41), (8, 42), (9, 18),
@@ -76,24 +76,25 @@ class GameScene(object):
         for event in pg.event.get():
 
             if event.type == pg.KEYDOWN:
-                if event.key == pg.K_ESCAPE:
-                    exit()
-                elif event.key == pg.K_p:
-                    self.status = not self.status
-                elif event.key == pg.K_s:
-                    self.regeneration()
-                elif event.key == pg.K_i:
-                    print self.get_pos_live_cells()
-                elif event.key == pg.K_b:
-                    self.borders()
+                self.check_key_pressed(event.key)
 
             if event.type == pg.MOUSEBUTTONDOWN:
-                for cell in self.cells:
-                    if cell.rect.collidepoint(event.pos[0], event.pos[1]):
-                        self.change_status(cell)
+                self.check_click_over_cells(event.pos)
 
-        if self.status:
+        if self.auto_generation:
             self.regeneration()
+
+    def check_key_pressed(self, key):
+        if key == pg.K_ESCAPE:
+            exit()
+        elif key == pg.K_p:
+            self.auto_generation = not self.auto_generation
+        elif key == pg.K_s:
+            self.regeneration()
+        elif key == pg.K_i:
+            print self.get_pos_live_cells()
+        elif key == pg.K_b:
+            self.borders()
 
     def borders(self):
         for cell in self.cells:
@@ -104,6 +105,11 @@ class GameScene(object):
             cell.draw(screen)
 
         pg.display.flip()
+
+    def check_click_over_cells(self, pos):
+        for cell in self.cells:
+            if cell.rect.collidepoint(pos[0], pos[1]):
+                self.change_status(cell)
 
     def get_pos_live_cells(self):
         return [cell.pos for cell in self.cells if cell.status]
@@ -116,54 +122,51 @@ class GameScene(object):
         for index, cell in enumerate(self.cells):
             neighbours = self.check_neighbours(index, cell)
             if cell.status:
-                if not len(neighbours) in [2, 3]:
+                if not neighbours in [2, 3]:
                     cells_to_update.append(cell)
             else:
-                if len(neighbours) in [3]:
+                if neighbours in [3]:
                     cells_to_update.append(cell)
 
         for cell in cells_to_update:
             self.change_status(cell)
 
-    def check_neighbours(self, index, cell):
-        pos_cell = index
-        neighbours = []
+    def check_neighbours(self, pos_cell, cell):
+        neighbours = 0
 
         if pos_cell > self.witdth_tiled:
             before_file = pos_cell - self.witdth_tiled
             if self.cells[before_file-1].status:
-                neighbours.append(1)
+                neighbours += 1
             if self.cells[before_file].status:
-                neighbours.append(1)
+                neighbours += 1
             if self.cells[before_file+1].status:
-                neighbours.append(1)
+                neighbours += 1
 
-        if pos_cell < (self.witdth_tiled * self.height_tiled
-                       - self.witdth_tiled - 1):
+        if pos_cell < len(self.cells) - self.witdth_tiled - 1:
             after_file = pos_cell + self.witdth_tiled
             if self.cells[after_file-1].status:
-                neighbours.append(1)
+                neighbours += 1
             if self.cells[after_file].status:
-                neighbours.append(1)
+                neighbours += 1
             if self.cells[after_file+1].status:
-                neighbours.append(1)
+                neighbours += 1
 
         if pos_cell > 0:
             before_cell = self.cells[pos_cell-1]
-            if before_cell.status and not cell.pos[1] == 0:
-                neighbours.append(1)
+            if before_cell.status and cell.pos[1] > 0:
+                neighbours += 1
 
         if pos_cell < len(self.cells) - 1:
             after_cell = self.cells[pos_cell+1]
-            if (after_cell.status and
-                    not cell.pos[1] == self.witdth_tiled-1):
-                neighbours.append(1)
+            if after_cell.status and cell.pos[1] < self.witdth_tiled - 1:
+                neighbours += 1
 
         return neighbours
 
 
 def main():
-    screen = pg.display.set_mode((640, 480))
+    screen = pg.display.set_mode((800, 600))
     pg.display.set_caption('Game of life')
     pg.init()
     clock = pg.time.Clock()
